ee259e46565310aeaf76a8e96803f26ebd7d5dfd
Hleb Stenin <hlebstenin@gmail.com>
2026-02-14 15:28:22 +0300

    feat: bind logger

--- src/logging.py
+++ src/logging.py
@@ -1,15 +1,14 @@
 import structlog
 
 
-def configure_structlog() -> None:
-    structlog.configure(
-        processors=[
-            structlog.processors.TimeStamper(fmt="iso", utc=True),
-            structlog.processors.add_log_level,
-            structlog.processors.JSONRenderer(),
-        ],
-    )
+structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso", utc=True),
+        structlog.processors.add_log_level,
+        structlog.processors.JSONRenderer(),
+    ],
+)
 
 
 def get_logger(name: str) -> structlog.stdlib.BoundLogger:
-    return structlog.get_logger(name)
+    return structlog.get_logger(name).bind(logger=name)
--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 from src.dataset import Dataset
 
-
 if __name__ == "__main__":
     dataset = Dataset()
 
